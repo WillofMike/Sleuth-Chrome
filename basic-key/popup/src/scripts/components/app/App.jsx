8d6dd5258cca3f5770bae7ca4ffcda4b175c4207
kylegibson15 <kylegibson15@gmail.com>
2017-12-07 14:26:22 -0700

    working popup speed test, posts data, displays spd & ip address

--- basic-key/popup/src/scripts/components/app/App.jsx
+++ basic-key/popup/src/scripts/components/app/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
       allSpeeds: [],
       currentSpeed: 0,
       packetIndex: 0,
-      message: ''
+      message: '',
     };
   }
 
@@ -33,13 +33,12 @@ class App extends Component {
     this.setState({ip: array[0]})
     response = await fetch(`https://galvanize-cors-proxy.herokuapp.com/https://infinite-beach-55234.herokuapp.com/tests/${array[0]}`)
     json = await response.json()
-    console.log(json[0]);
     this.setState({data: json[0]})
   }
 
   async addItem(data) {
-    this.setState({message: 'Adding this item, bitch'})
-    const response = await fetch("https://galvanize-cors-proxy.herokuapp.com/https://infinite-beach-55234.herokuapp.com/tests/", {
+    this.setState({message: this.state.ip})
+    const response = await fetch(`https://galvanize-cors-proxy.herokuapp.com/https://infinite-beach-55234.herokuapp.com/tests/`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
@@ -47,89 +46,82 @@ class App extends Component {
         'Accept': 'application/json'
       }
     })
+    this.setState({message: response})
     this.componentDidMount();
   }
+  async runTheTest(){
 
-  runTest() {
-    if (this.state.allSpeeds.length < 10) {
-      speedTest(this.state.packetIndex).then(speed => {
-        if (speed === 0) {
-          console.log('no internet');
-          this.setState({calculating: false})
-        } else {
-          let newSpeeds = [...this.state.allSpeeds];
-          newSpeeds.push(Number(speed))
-          let newIndex = this.state.packetIndex;
-          newIndex++
-          this.setState({allSpeeds: newSpeeds, currentSpeed: speed, packetIndex: newIndex})
-        }
-      })
-    } else if (this.state.allSpeeds.length >= 10) {
-      this.setState({message: 'Line 75 ran'});
-      let newTest = this.state.testResults;
-      let data = [...this.state.allSpeeds];
-
-      data.sort((a, b) => a - b);
-      let lowMiddle = Math.floor((data.length - 1) / 2);
-      let highMiddle = Math.ceil((data.length - 1) / 2);
-      let median = (data[lowMiddle] + data[highMiddle]) / 2;
-
-      newTest.speed = median.toFixed(2)
-      let newArr = [];
-      let postData = {
-        ip: newTest.IP,
-        dl_speed: newTest.speed,
-        name: newTest.isp,
-        lat: newTest.lat,
-        long: newTest.lon
+    speedTest(this.state.packetIndex).then(speed => {
+      if (speed === 0) {
+        this.setState({calculating: false, message: '56: Speed is 0'})
+      } else {
+        let newSpeeds = [...this.state.allSpeeds];
+        newSpeeds.push(Number(speed))
+        let newIndex = this.state.packetIndex;
+        newIndex++
+        this.setState({allSpeeds: newSpeeds, currentSpeed: speed, packetIndex: newIndex})
       }
-      this.addItem(postData);
-      this.setState({testResults: newTest, calculating: false, allSpeeds: newArr, packetIndex: 0})
-      console.log(newTest);
+    })
+  }
+  finishTheTest(){
+    this.setState({message: 'Line 65 ran'});
+    let newTest = this.state.testResults;
+    this.setState({message: newTest.IP})
+    let data = [...this.state.allSpeeds];
+
+    data.sort((a, b) => a - b);
+    let lowMiddle = Math.floor((data.length - 1) / 2);
+    let highMiddle = Math.ceil((data.length - 1) / 2);
+    let median = (data[lowMiddle] + data[highMiddle]) / 2;
+
+    newTest.speed = median.toFixed(2)
+    let newArr = [];
+    let postData = {
+      ip: this.state.ip,
+      dl_speed: newTest.speed,
+      name: newTest.isp,
+      lat: newTest.lat,
+      long: newTest.lon
+    }
+    this.addItem(postData);
+    this.setState({testResults: newTest, calculating: false, allSpeeds: newArr, packetIndex: 0})
+  }
+  runTest() {
+    console.log('line 88, runTest');
+    if (this.state.allSpeeds.length < 10) {
+      this.runTheTest()
+    } else if (this.state.allSpeeds.length >= 10) {
+      this.finishTheTest()
     }
   }
-
-  calculate(e) {
-    e.preventDefault();
-    this.setState({calculating: true});
-    this.getISP();
+  toggleCalculate() {
+    if(this.state.calculating == true) {
+      this.setState({calculating: false})
+    }else{
+      this.setState({calculating: true})
+    }
   }
-
-  componentWillMount() {
-    if (this.calculate) {
-      // this.setState({message: 'isCalculating'});
-      for (var i = 0; i < 11; i++) {
-
-        this.runTest();
-      }
+  // calculate(){
+  //   // this.preventDefault();
+  //   this.setState({calculating: true});
+  //   // this.getISP();
+  // }
+  async componentWillMount() {
+    this.setState({message: 'line 109'})
+    console.log('line 103, componentWillMount');
+    if (this.state.calculating) {
+      this.runTest();
     }
   }
 
   render() {
     return (<div className='p-3 mb-2 bg-dark'>
-      <h5>Current IP address: {this.state.ip}</h5>
-      <h5>{this.state.data.dl_speed}</h5>
-      <h6>testResults speed: {this.state.testResults.speed}</h6>
-      <form onSubmit={this.componentDidMount()}>
-        <button onClick={this.calculate.bind(this)} className="btn btn-primary">Run Sleuth</button>
-      </form>
-      <p>{this.state.message}</p>
-      <p>all speeds: {this.state.allSpeeds}</p>
+      <h6>IP address: {this.state.ip}</h6>
+      {this.state.calculating ? this.runTest() : null}
+      <h5>Download Speed: {this.state.testResults.speed}</h5>
+      <button onClick={this.toggleCalculate.bind(this)} type="submit" className="btn btn-primary">Run Sleuth</button>
     </div>);
   }
 }
-// expectedSpeed: null,
-// toggleInputField: false,
-// testResults: {
-//   IP: '',
-//   isp: '',
-//   speed: 0,
-//   lat: 0,
-//   lon: 0
-// },
-// calculating: false,
-// allSpeeds: [],
-// currentSpeed: 0,
-// packetIndex: 0,
-// message: '',
+
 export default App;
