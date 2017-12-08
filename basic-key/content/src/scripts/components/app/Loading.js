56d7b0ab2588683c3b1a6cc5303fbab86f6d3ba0
kylegibson15 <kylegibson15@gmail.com>
2017-12-07 18:02:59 -0700

    loading gif component

---
+++ basic-key/content/src/scripts/components/app/Loading.js
@@ -0,0 +1,13 @@
+// import React, {Component} from 'react';
+//
+// class Loading extends Component {
+//
+//   render() {
+//     return (<div>
+//       <h6>gif</h6>
+//       <img src={this.props.gif}/>
+//     </div>)
+//   }
+// }
+//
+// export default Loading
